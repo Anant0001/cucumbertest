a3b083a8b18d3d63943ef6efbc3bb0638ecdac3b
Tanuj Vishnoi <tanujkumarvishnoi@qainfotech.com>
2021-10-04 23:31:59 +0530

    Update Testblabla.java

--- src/test/java/packtest/Testblabla.java
+++ src/test/java/packtest/Testblabla.java
@@ -2,10 +2,11 @@ package packtest;
 
 import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resource/flipkart.feature")
+@CucumberOptions(features="src/test/resources/feature/flipkart.feature")
 public class Testblabla {
 
 }
